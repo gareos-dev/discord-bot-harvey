288454840a824feaa13ff2844855c34478b85782
gareos-dev <gareos.dev@gmail.com>
2021-01-21 17:23:32 +0100

    create new console project and .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace discord_bot_harvey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
